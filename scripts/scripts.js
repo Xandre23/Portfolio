20914e45d1018924abcea6bc76c836a0d78618e9
Xandre <xandredepaula2312@gmail.com>
2022-02-23 20:51:22 -0300

    Arrumando o portfolio

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+/**********************typing animation ============== */
+
+var typed = new Typed(".typing",{
+    strings:["Desenvolvedor Back-end","Desenvolvedor Web", "Desenvolvedor Back-end"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
